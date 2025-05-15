b42b44253bf223bd1602c105bce68cf03ce01f06
saulo <78312710+Saulo217@users.noreply.github.com>
2025-05-15 15:42:09 -0300

    feat: added metrics to parallel jobs

--- basics.sh
+++ basics.sh
@@ -5,3 +5,9 @@ parallel echo ::: a b c
 
 # use max of cpu core as possible to run jobs in parallel
 parallel -j+0 echo ::: a b c d e f g h i j
+
+# show estimated time to conclude each job
+parallel -j+0 --eta echo ::: a b c d e f g h i j
+
+# show the progress
+parallel -j+0 --progress echo ::: a b c d e f g h i j
